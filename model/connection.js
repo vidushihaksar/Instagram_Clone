0ad72fff63399befa271ef9cf3f2a8d008cffdb1
Vidushi <vidushi@Vishals-MacBook-Air.local>
2020-09-08 15:39:09 +0530

    added users

---
+++ model/connection.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Haksar@1999',
+    database: 'insta_pp'
+})
+
+connection.connect();
+module.exports = connection;
+
+//in case of error "Client does not support authentication protocol requested by server; consider upgrading MySQL client'," use this command
+
+// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password'
+// flush privileges;
---
+++ utility/createTables.sql
@@ -0,0 +1,16 @@
+-- 1. GO to mysql client
+-- 2. create database  insta_pp;
+-- 3. create sql script 
+-- 4. go back to mysql 
+        -- type source sqlscript path 
+        -- to check=> show tables
+CREATE TABLE IF NOT EXISTS user(
+    uid VARCHAR(80) PRIMARY KEY,
+    handle VARCHAR(30) NOT NULL UNIQUE,
+    email_id VARCHAR(50) UNIQUE,
+    phone BIGINT(10) UNIQUE,
+    bio VARCHAR(150),
+    is_verfied BOOLEAN DEFAULT false,
+    is_public BOOLEAN DEFAULT true,
+    p_img_url VARCHAR(255) 
+);
