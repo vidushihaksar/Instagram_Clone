b8e6c34a49d7a69ef4f861f8092467c6f468acb2
vidushi <haksarvidushi@gmail.com>
2020-09-09 19:11:56 +0530

    added accept/reject request

--- model/userFollowerModel.js
+++ model/userFollowerModel.js
@@ -14,4 +14,35 @@ const createRequest = function (mappingObj) {
         });
     })
 }
+
+// user ke ui
+const acceptRequestQ = function (user_id, follower_id) {
+    return new Promise(function (resolve, reject) {
+        db.query(`UPDATE user_follower SET is_pending=0 WHERE user_id="${user_id}" 
+        AND follower_id="${follower_id}"`, function (err, result) {
+            // Neat!
+            if (err) {
+                reject(err)
+            } else {
+                resolve(result);
+            }
+        });
+    })
+}
+// user ke ui
+const rejectRequestQ = function (user_id, follower_id) {
+    return new Promise(function (resolve, reject) {
+        db.query(`DELETE from user_follower  WHERE user_id="${user_id}" 
+        AND follower_id="${follower_id} AND is_pending=1"`, function (err, result) {
+            // Neat!
+            if (err) {
+                reject(err)
+            } else {
+                resolve(result);
+            }
+        });
+    })
+}
 module.exports.createRequest = createRequest;
+module.exports.acceptRequestQ = acceptRequestQ;
+module.exports.rejectRequestQ = rejectRequestQ;
