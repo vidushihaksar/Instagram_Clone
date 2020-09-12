70b789e8daf18a8ccaaa5820845bbf9185097408
vidushi <haksarvidushi@gmail.com>
2020-09-13 00:41:10 +0530

    added frontend for profile page

--- model/userFollowerModel.js
+++ model/userFollowerModel.js
@@ -43,6 +43,20 @@ const rejectRequestQ = function (user_id, follower_id) {
         });
     })
 }
+const getAllFolId = function (user_id) {
+    return new Promise(function (resolve, reject) {
+        db.query(`SELECT * from user_follower WHERE user_id="${user_id}"`, function (err, result) {
+            // Neat!
+            if (err) {
+                reject(err)
+            } else {
+                resolve(result);
+            }
+        });
+    })
+} 
+
 module.exports.createRequest = createRequest;
 module.exports.acceptRequestQ = acceptRequestQ;
 module.exports.rejectRequestQ = rejectRequestQ;
+module.exports.getAllFolId = getAllFolId;
